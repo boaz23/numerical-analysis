b4d56aa553e0542fc04b6a7c07f992f0fe0c4f3d
Boaz Rozovski <boazrozo@post.bgu.ac.il>
2021-02-11 22:50:38 +0200

    Q4A M refactor - preparing for general M for any d

--- assignment4A.py
+++ assignment4A.py
@@ -37,7 +37,8 @@ class Assignment4A:
         Here goes any one time calculation that need to be made before 
         solving the assignment for specific functions. 
         """
-        self.M3 = torch.Tensor(
+        self.Ms = [self.build_M(d) for d in range(13)]
+        self.Ms[3] = torch.Tensor(
             [[-1, +3, -3, +1],
              [+3, -6, +3, 0],
              [-3, +3, 0, 0],
@@ -66,22 +67,24 @@ class Assignment4A:
         -------
         a function:float->float that fits f between a and b
         """
+        start_time = time.time()
         n = 1000
-        M_d = self.M3
+        M = self.Ms[d] if d < len(self.Ms) else self.build_M(d)
+        M = self.Ms[3]
 
-        time_took = time.time()
-        fit_func = self.fit_core(f, a, b, n, d, M_d)
-        time_took = time.time() - time_took
+        fit_func = self.fit_core(f, a, b, n, 3, M)
+        time_took = time.time() - start_time
 
         time_left = maxtime - time_took - 0.01
         # the estimated amount of times we can calculate 5000 points in the time left while giving some buffer.
         n = (time_left / (time_took * 2 + 0.005)) * 5000
         n = int(min((b - a) * 5000, n))
         if n >= 2000:
-            fit_func = self.fit_core(f, a, b, n, d, M_d)
+            time_left = time.time() - start_time
+            fit_func = self.fit_core(f, a, b, n, 3, M)
         return fit_func
 
-    def fit_core(self, f: callable, a: float, b: float, n: int, d: int, M_d):
+    def fit_core(self, f: callable, a: float, b: float, n: int, d: int, M):
         #x_samples = np.concatenate([[a], (np.random.random(n - 2) * (b - a)) + a, [b]])
         #x_samples.sort()
         x_samples = np.linspace(a, b, n)
@@ -95,10 +98,10 @@ class Assignment4A:
         d_total = functools.reduce(sum_d, range(1, n), 0)
 
         t_dis = torch.Tensor([dis[i] / d_total for i in range(n)])
-        T = torch.stack([t_dis ** k for k in range(3, -1, -1)]).T
+        T = torch.stack([t_dis ** k for k in range(d, -1, -1)]).T
         P = torch.stack([torch.Tensor(x_samples), torch.Tensor(y_samples)]).T
-        C = M_d.inverse().mm((T.T.mm(T)).inverse()).mm(T.T).mm(P)
-        bezier_curve = M_d.mm(C).T
+        C = M.inverse().mm((T.T.mm(T)).inverse()).mm(T.T).mm(P)
+        bezier_curve = M.mm(C).T
         bezier_x_poly = np.poly1d(converted_to_np_float32(bezier_curve[0]))
         bezier_y_poly = np.poly1d(converted_to_np_float32(bezier_curve[1]))
 
@@ -128,6 +131,9 @@ class Assignment4A:
             return bezier_y_poly(find_t(x))
 
         return fit_func
+
+    def build_M(self, d: int):
+        M = np.zeros((d + 1, d + 1))
 
 ##########################################################################
 
