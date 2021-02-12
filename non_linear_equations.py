527ebd0ae5290d44e237112d652fc3184a0912c5
Boaz Rozovski <boazrozo@post.bgu.ac.il>
2021-02-12 14:45:49 +0200

    Dead code removal

--- non_linear_equations.py
+++ non_linear_equations.py
@@ -4,38 +4,6 @@ import math
 def is_root(y, maxerr):
     return abs(y) < maxerr
 
-def is_same_x(x1, x2, epsilon):
-    return abs(x1 - x2) < epsilon
-
-def find_roots_from_der_roots(f, a, b, d_roots, maxerr):
-    roots = []
-
-    prev_a = a
-    f_prev_a = f(a)
-    if is_root(f_prev_a, maxerr):
-        roots.append(prev_a)
-
-    for i in range(len(d_roots)):
-        dr = d_roots[i]
-        f_dr = f(dr)
-        if is_root(f_dr, maxerr):
-            roots.append(dr)
-        elif (f_prev_a < 0) != (f_dr < 0):
-            roots.append(regula_falsi(f, prev_a, dr, maxerr))
-        else:
-            pass
-        prev_a = dr
-        f_prev_a = f_dr
-
-    f_b = f(b)
-    if is_root(f_b, maxerr):
-        roots.append(b)
-    elif (f_prev_a < 0) != (f_b < 0):
-        roots.append(regula_falsi(f, prev_a, b, maxerr))
-    else:
-        pass
-    return roots
-
 
 def regula_falsi(f, a, b, maxerr):
     c = b
@@ -113,21 +81,3 @@ def unique_roots_filter(f, roots, maxerr):
             i += 1
         unique_roots.append(min_x)
     return unique_roots
-
-def find_roots_brute_force(f: callable, a: float, b: float, maxerr: float):
-    epsilon = maxerr / 10
-    intervals_stack = [(a, b)]
-    roots = []
-    while len(intervals_stack) > 0:
-        a, b = intervals_stack.pop()
-        if a > b:
-            continue
-        if is_same_x(a, b, epsilon):
-            fa = f(a)
-            if is_root(fa, maxerr):
-                roots.append((a, fa))
-        else:
-            mid = (a + b) / 2
-            intervals_stack.append((mid, b))
-            intervals_stack.append((a, mid))
-    return roots
--- poly_funcs.py
+++ poly_funcs.py
@@ -1,59 +1,3 @@
-import numpy as np
-import non_linear_equations
-
-# noinspection PyTypeChecker,PyUnresolvedReferences,PyArgumentList
-def find_roots(p: np.poly1d, start, end, maxerr=0.001):
-    global base_poly_roots_finders
-    coefs = p.coefficients
-    d = p.order
-    if d < len(base_poly_roots_finders):
-        return base_poly_roots_finders[d](start, end, *coefs)
-    dp_roots = find_roots(np.polyder(p), start, end, maxerr)
-    return non_linear_equations.find_roots_from_der_roots(p, start, end, dp_roots, maxerr)
-
-def _find_roots_poly_constant(start, end, constant):
-    if constant == 0:
-        raise Exception("Zero polynomial has infinite amount roots")
-    return []
-def _find_roots_poly_linear_line(start, end, slope, constant):
-    if slope == 0:
-        return _find_roots_poly_constant(start, end, constant)
-    root = (-constant) / slope
-    return _roots_list(start, end, root)
-def _find_roots_poly_quadratic(start, end, a, b, c):
-    if a == 0:
-        return _find_roots_poly_linear_line(start, end, b ,c)
-    return _find_roots_poly_quadratic_core(a, b, c, end, start)
-
-def _find_roots_poly_quadratic_core(a, b, c, end, start):
-    delta = (b ** 2) - 4 * a * c
-    if delta < 0:
-        return []
-    elif delta == 0:
-        root = -b / (2 * a)
-        return _roots_list(start, end, root)
-    else:
-        return _find_roots_poly_quadratic_2_roots(a, b, delta, end, start)
-
-def _find_roots_poly_quadratic_2_roots(a, b, delta, end, start):
-    delta = np.sqrt(delta)
-    root0 = (-b + delta) / (2 * a)
-    root1 = (-b - delta) / (2 * a)
-    root0, root1 = min(root0, root1), max(root0, root1)
-    return _roots_list(end, start, root0, root1)
-
-
-base_poly_roots_finders = [
-    _find_roots_poly_constant,
-    _find_roots_poly_linear_line,
-    _find_roots_poly_quadratic
-]
-
-
-def _roots_list(end, start, *roots):
-    return [root for root in roots if start <= root <= end]
-
-
 def poly_to_string(p):
     coefficients = p.coefficients
     highest = coefficients[0]
@@ -97,6 +41,4 @@ def coef_to_string(coef):
     return s
 
 if __name__ == "__main__":
-    #print(find_roots(np.poly1d([1, 0, -1, 0]), -10, 10))
-    #print(find_roots(np.poly1d(np.random.random(11)), -10, 10))
     pass
